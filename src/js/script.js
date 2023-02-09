cadda7a493cfbc6a22daa64becfa359abab710eb
erlankusain <erlankusain@yandex.ru>
2023-02-09 11:08:58 +0600

    slick slider add and about section

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('.promo__slider').slick({
+    speed: 1200,
+    prevArrow: '<button type="button" class="slick-prev"><img src= "../img/icons/left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src= "../img/icons/right.svg"></button>'
+  });
+});
