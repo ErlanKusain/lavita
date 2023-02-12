b10da401da8bdb85080f7886044c62d7e96058f8
erlankusain <erlankusain@yandex.ru>
2023-02-12 12:04:08 +0600

    tabs add

--- src/js/script.js
+++ src/js/script.js
@@ -4,4 +4,14 @@ $(document).ready(function(){
     prevArrow: '<button type="button" class="slick-prev"><img src= "../img/icons/left.svg"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src= "../img/icons/right.svg"></button>'
   });
+
+  $('ul.price__tabs').on('click', 'li:not(.price__tab_active)', function() {
+    $(this)
+      .addClass('price__tab_active').siblings().removeClass('price__tab_active')
+      .closest('div.container').find('div.price__content').removeClass('price__content_active').eq($(this).index()).addClass('price__content_active');
+  });
+
+  
 });
+
+
